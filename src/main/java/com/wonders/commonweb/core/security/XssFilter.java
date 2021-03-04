915e29d39104f559f4e392c44ed2bf6bb8b1bfb1
wangjiaming <13422192925@163.com>
2021-03-04 16:54:49 +0800

    add security filter

---
+++ src/main/java/com/wonders/commonweb/core/security/XssFilter.java
@@ -0,0 +1,32 @@
+package com.wonders.commonweb.core.security;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@WebFilter(urlPatterns = "*", filterName = "userFilter")
+@Slf4j
+public class XssFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        request=new XssHttpServletRequestWrapper((HttpServletRequest) request);
+//        System.out.println(request.getParameter("a"));
+        chain.doFilter(new XssHttpServletRequestWrapper((HttpServletRequest) request), response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("加载Xss安全拦截器");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
